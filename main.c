03ef3fd726b0ceee357989d3b80073c4f5391f95
SiJayAlex <Sheeplex8652@yandex.ru>
2020-02-14 15:06:40 +0700

    Add .clang-format

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-	printf(" Hello, World\n");
-	system("pause");
-	return 0;
+int main()
+{
+    printf(" Hello, World\n");
+    system("pause");
+    return 0;
 }
